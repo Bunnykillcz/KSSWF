dd9ab2baa84c3f9dd7bc56b1ee899c6b074a0d06
Nikola Nejedlý | Bunnykill <bunnykillcze@gmail.com>
2017-09-25 18:36:30 +0200

    Initial commit; Clean start;

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KSSWF
 KyberSoft Simple Webpage Framework
+All the readme informations necessary are included in index.php
+License - CC-BY-SA 4.0; 
+Enjoy !
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="cs-CZ">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=0.8">
+<?php 
+// INFORMATIONS
+//-------------------------------------------------------------------------------------------------- //
+// KyberSoft Simple Website Framework aka. KSSWF
+// Version: 0.0.1 
+// Made by Nikola Nejedlý | KyberSoft (c) 2017
+// Author: http://nejedniko.tk
+// Origin: https://github.com/Bunnykillcz/KSSWF
+// Last update: 25.09.2017
+// License: CC-BY-SA 4.0  (https://creativecommons.org/licenses/by/4.0/legalcode)
+//-------------------------------------------------------------------------------------------------- //
+// Copy: Nikola Nejedlý
+// Purpose: KSSWF 
+// Web: http://nejedniko.tk
+//-------------------------------------------------------------------------------------------------- //
+
+
+//Change these:
+$title   = "TITLE";
+$tags    = "tag, tag, tag";
+$favicon = "img/favicon.ico";
+$
+
+
+
+?>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet"/> 
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Monofett" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Cormorant+SC" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet"/> 
+<link href="https://fonts.googleapis.com/css?family=Aladin" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC" rel="stylesheet" /> 
+
+
+<title>TITLE</title>
+
+</head>
+<body>
+</body>
+<footer>
+</footer>
