148fb564e9296724163f1797b44ca9081c3f1a95
Nikola Nejedlý | Bunnykill <bunnykillcze@gmail.com>
2017-10-19 21:17:29 +0200

    javascript jquery installed; highlightjs installed; #2nd part

---
+++ func/js_load.php
@@ -0,0 +1,24 @@
+<?php 
+//Necessary style links?
+?>
+<link rel="stylesheet" href="./javascript/highlight/styles/default.css">
+
+
+
+<?php 
+//Java Script links
+?>
+<script language="JavaScript" type="text/javascript" src="./javascript/jquery-3.2.1.min.js"></script>
+<script language="JavaScript" type="text/javascript" src="./javascript/htmlspecialchars.js"></script>
+
+<script language="JavaScript" type="text/javascript" src="./javascript/highlight/highlight.pack.js"></script>
+<script>
+hljs.configure({
+	useBR: true,
+	tabReplace: '    ', 
+	classPrefix: '',
+})
+</script>
+<script>
+hljs.initHighlightingOnLoad();
+</script>
---
+++ javascript/htmlspecialchars.js
@@ -0,0 +1,4 @@
+$(function() {
+   var $pre = $('pre code');
+   $pre.html($pre.html().replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#039;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+});
