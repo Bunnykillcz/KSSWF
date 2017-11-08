cc310c6562ce0b03a4db7210df344d92ff06da33
Nikola Nejedlý | Bunnykill <bunnykillcze@gmail.com>
2017-11-08 01:40:56 +0100

    Template orange update; test page updated; get started updated; @ntb

--- pages/test.php
+++ pages/test.php
@@ -1,14 +1,71 @@
+<?php  ?>
+
 <h1>Lorem Ipsum h1</h1>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.</p>
-<p>Nam sed tellus id magna elementum tincidunt. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. In rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. Integer vulputate sem a nibh rutrum consequat. Nunc auctor. Etiam commodo dui eget wisi. Nunc dapibus tortor vel mi dapibus sollicitudin. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nulla quis diam. Vivamus luctus egestas leo. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.</p>
-<p>Nulla pulvinar eleifend sem. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Praesent in mauris eu tortor porttitor accumsan. Donec vitae arcu. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nullam sit amet magna in magna gravida vehicula. Aenean vel massa quis mauris vehicula lacinia. Aliquam erat volutpat. Pellentesque pretium lectus id turpis. Duis risus.</p>
+<p>
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. 
+<img style="height:240px;" src="https://images.pexels.com/photos/225231/pexels-photo-225231.jpeg" alt="test image"/>
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.
+</p><p>
+Nam sed tellus id magna elementum tincidunt. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. In rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. Integer vulputate sem a nibh rutrum consequat. Nunc auctor. Etiam commodo dui eget wisi. Nunc dapibus tortor vel mi dapibus sollicitudin. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nulla quis diam. Vivamus luctus egestas leo. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.
+</p><p>
+Nulla pulvinar eleifend sem. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Praesent in mauris eu tortor porttitor accumsan. Donec vitae arcu. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nullam sit amet magna in magna gravida vehicula. Aenean vel massa quis mauris vehicula lacinia. Aliquam erat volutpat. Pellentesque pretium lectus id turpis. Duis risus.
+</p>
+
 <h2>Lorem Ipsum h2</h2>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.</p>
-<p>Nam sed tellus id magna elementum tincidunt. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. In rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. Integer vulputate sem a nibh rutrum consequat. Nunc auctor. Etiam commodo dui eget wisi. Nunc dapibus tortor vel mi dapibus sollicitudin. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nulla quis diam. Vivamus luctus egestas leo. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.</p>
+<p>
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.
+</p><p>
+Nam sed tellus id magna elementum tincidunt. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. In rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. Integer vulputate sem a nibh rutrum consequat. Nunc auctor. Etiam commodo dui eget wisi. Nunc dapibus tortor vel mi dapibus sollicitudin. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nulla quis diam. Vivamus luctus egestas leo. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.
+</p>
+
 <h3>Ipsum Dolor h3</h3>
-<p>Ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.</p>
+<p>
+Ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.
+<br/>
+<div class="floatright"><?php button("Button","#",false,0,0); ?> <br/> .floatright</div>
+<ol> OL 01
+	<li>LI 01</li>
+	<li>LI 02</li>
+	<li>LI 03</li>
+	<ol> OL 02
+		<li>LI 01</li>
+		<li>LI 02</li>
+		<li>LI 03</li>
+		<ol> OL 03
+			<li>LI 01</li>
+			<li>LI 02</li>
+			<li>LI 03</li>
+		</ol>
+	</ol>
+</ol>
+</p>
+
 <h4>Dolor Sit Amet h4</h4>
-<p>Dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.</p>
-<?php 
-//generate HOME stuff like NEWS?
-?>
+<p>
+Dolor sit amet, consectetuer adipiscing elit. Integer tempor. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Quisque porta. Integer vulputate sem a nibh rutrum consequat. Morbi scelerisque luctus velit. Nunc auctor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam in lorem sit amet leo accumsan lacinia. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nunc dapibus tortor vel mi dapibus sollicitudin. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Phasellus faucibus molestie nisl. Nulla non lectus sed nisl molestie malesuada. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.
+<br/>
+<ul> UL 01
+	<li>LI 01</li>
+	<li>LI 02</li>
+	<li>LI 03</li>
+	<ul> UL 02
+		<li>LI 01</li>
+		<li>LI 02</li>
+		<li>LI 03</li>
+		<ul> UL 03
+			<li>LI 01</li>
+			<li>LI 02</li>
+			<li>LI 03</li>
+		</ul>
+	</ul>
+</ul>
+</p>
+<br/>
+
+<h4>Basic tags: </h4>
+<p>
+<br/><strong>STRONG</strong>
+, <b> B </b>, <i> I </i>, <u> U </u>
+</p>
+
+
